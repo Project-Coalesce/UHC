776ef0cfba237a89d03d061053973394a8e1da01
Mariell Hoversholm <proximyst@proximyst.com>
2017-04-21 23:45:54 +0200

    Actually make dead players spectators. TODO: Add customizability.

--- src/main/java/com/coalesce/uhc/eventhandlers/DeathHandler.java
+++ src/main/java/com/coalesce/uhc/eventhandlers/DeathHandler.java
@@ -4,6 +4,7 @@ import com.coalesce.uhc.GameState;
 import com.coalesce.uhc.UHC;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
@@ -20,7 +21,8 @@ public class DeathHandler implements Listener {
         } else {
             event.getEntity().sendMessage(ChatColor.RED + "You'll now be in spectator mode. Don't ghost or such!");
         }
-
+        event.getEntity().setGameMode(GameMode.SPECTATOR);
+        
         if (Bukkit.getServer().getOnlinePlayers().stream().allMatch(player -> player.getMetadata("wasAlive").isEmpty())) {
             event.setDeathMessage(ChatColor.YELLOW + " - First death: " + event.getDeathMessage());
         } else {
