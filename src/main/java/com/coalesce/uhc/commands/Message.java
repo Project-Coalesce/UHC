60ab80a5de5f2b0b52de7a86c31b75c21e935d61
deprilula28 <deprilula28@gmail.com>
2017-04-22 20:32:44 -0300

    Formatted a bit to match proxis very important needs

--- src/main/java/com/coalesce/uhc/commands/Message.java
+++ src/main/java/com/coalesce/uhc/commands/Message.java
@@ -25,8 +25,7 @@ public class Message implements Listener {
         UHC.getInstance().registerListener(this);
     }
 
-    @EventHandler
-    public void leave(PlayerQuitEvent event){
+    @EventHandler public void leave(PlayerQuitEvent event) {
         if(lastMessaged.containsKey(event.getPlayer())) lastMessaged.remove(event.getPlayer());
     }
 
