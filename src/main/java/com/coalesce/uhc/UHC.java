b43b1cc73a2f27cc95673f641ceba3c5882f8699
deprilula28 <deprilula28@gmail.com>
2017-04-24 18:16:41 -0300

    Add a proper config loader

--- src/main/java/com/coalesce/uhc/UHC.java
+++ src/main/java/com/coalesce/uhc/UHC.java
@@ -3,13 +3,16 @@ package com.coalesce.uhc;
 import com.coalesce.plugin.CoPlugin;
 import com.coalesce.uhc.configuration.MainConfiguration;
 import com.coalesce.uhc.eventhandlers.*;
+import com.coalesce.uhc.utilities.MainConfigWriter;
 import com.google.gson.Gson;
 import lombok.Getter;
 import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.Arrays;
 import java.util.logging.Level;
 
@@ -21,10 +24,18 @@ public class UHC extends CoPlugin {
         instance = this;
 
 		try {
-			mainConfig = new Gson().fromJson(new FileReader("config.json"), MainConfiguration.class);
+			mainConfig = new Gson().fromJson(new FileReader(getDataFolder().getAbsolutePath() + File.separatorChar +
+                    "config.json"), MainConfiguration.class);
 		} catch (FileNotFoundException e) {
-		    e.printStackTrace();
+            getLogger().log(Level.INFO, "Setting up configs folder...");
+
+            mainConfig = new MainConfiguration(
+                    false, 2000, 10, 120, 100,
+                    30
+            );
+            MainConfigWriter.writeMainConfig(getDataFolder(), mainConfig);
         }
+
 		Bukkit.getWorlds().forEach(world -> world.setGameRuleValue("NaturalRegeneration", "false")); // Make sure it's hardcore.
 
         new CommandHandler(this);
