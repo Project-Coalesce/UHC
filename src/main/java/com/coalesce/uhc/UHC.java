e1c3ae8173674a6994f5a56b97ba094c5b147d30
Mariell Hoversholm <proximyst@proximyst.com>
2017-04-27 07:43:25 +0200

    Added rules file and made sure datafolder exists

--- src/main/java/com/coalesce/uhc/UHC.java
+++ src/main/java/com/coalesce/uhc/UHC.java
@@ -5,6 +5,7 @@ import com.coalesce.uhc.configuration.MainConfiguration;
 import com.coalesce.uhc.eventhandlers.*;
 import com.coalesce.uhc.utilities.MainConfigWriter;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import lombok.Getter;
 import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
@@ -18,16 +19,22 @@ import java.util.logging.Level;
 public class UHC extends CoPlugin {
     @Getter private static UHC instance;
     @Getter private MainConfiguration mainConfig;
+    @Getter private Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().serializeNulls().create();
+    @Getter private File rulesFile;
 
     @Override
     public void onPluginEnable() /* throws Exception - We ain't throwing shit. */ {
         instance = this;
         displayName = "CoalesceUHC";
 
-		try {
-			mainConfig = new Gson().fromJson(new FileReader(getDataFolder().getAbsolutePath() + File.separatorChar +
+        if (!getDataFolder().exists()) {
+            getDataFolder().mkdirs();
+        }
+
+        try {
+            mainConfig = gson.fromJson(new FileReader(getDataFolder().getAbsolutePath() + File.separatorChar +
                     "config.json"), MainConfiguration.class);
-		} catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             getLogger().log(Level.INFO, "Setting up configs folder...");
 
             mainConfig = new MainConfiguration(
@@ -37,10 +44,15 @@ public class UHC extends CoPlugin {
             MainConfigWriter.writeMainConfig(getDataFolder(), mainConfig);
         }
 
-		Bukkit.getWorlds().forEach(world -> world.setGameRuleValue("NaturalRegeneration", "false")); // Make sure it's hardcore.
+        rulesFile = new File(getDataFolder(), "rules.json");
+        if (!rulesFile.exists()) {
+            saveResource("rules.json", true);
+        }
+
+        Bukkit.getWorlds().forEach(world -> world.setGameRuleValue("NaturalRegeneration", "false")); // Make sure it's hardcore.
 
         new CommandHandler(this);
         Arrays.asList(new Listener[]{new DeathHandler(), new ArcheryHandler(), new GameInitializeHandler(), new JoinQuitHandlers(),
-            new MessageHandler()}).forEach(this::registerListener);
+                new MessageHandler()}).forEach(this::registerListener);
     }
 }
