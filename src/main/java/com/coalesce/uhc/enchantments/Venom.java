08bf8e133a8fc2c0a8b411cf98cb6e6a5a05749b
deprilula28 <deprilula28@gmail.com>
2017-04-30 01:22:30 -0300

    Make venom

--- src/main/java/com/coalesce/uhc/enchantments/Venom.java
+++ src/main/java/com/coalesce/uhc/enchantments/Venom.java
@@ -1,7 +1,18 @@
 package com.coalesce.uhc.enchantments;
 
+import static com.coalesce.uhc.utilities.BukkitRunnableWrapper.bukkitRunnable;
+
+import com.coalesce.uhc.UHC;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.enchantments.EnchantmentTarget;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import java.util.List;
 
 import static com.coalesce.uhc.utilities.Statics.colour;
 
@@ -9,6 +20,8 @@ import static com.coalesce.uhc.utilities.Statics.colour;
  * Applies potion upon the attacked entity.
  */
 public class Venom extends CustomEnchant {
+    private List<Player> cooldown;
+
     public Venom() {
         super(1501);
     }
@@ -22,5 +35,20 @@ public class Venom extends CustomEnchant {
                 Material.STONE_SWORD,
                 Material.WOOD_SWORD
         };
+    }
+
+    @EventHandler
+    public void onEntityDamagedByEntity(EntityDamageByEntityEvent event) {
+        if (event.getDamager() instanceof Player && event.getEntity() instanceof LivingEntity) {
+            Player player = (Player) event.getDamager();
+
+            if (player.getInventory().getItemInMainHand().containsEnchantment(this) && !cooldown.contains(player)) {
+                int level = player.getInventory().getItemInMainHand().getEnchantmentLevel(this);
+                ((LivingEntity) event.getEntity()).addPotionEffect(new PotionEffect(PotionEffectType.POISON, level * 6, level + 1), true);
+
+                cooldown.add(player);
+                Bukkit.getScheduler().runTaskLater(UHC.getInstance(), () -> cooldown.remove(player), 100); //5s
+            }
+        }
     }
 }
